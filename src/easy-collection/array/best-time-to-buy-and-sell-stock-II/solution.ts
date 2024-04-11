3fe26feb9ed36eb96d35d0008caa5b9f7d759a03
jadigar <whakawa@outlook.com>
2024-04-11 22:46:11 +0500

    best-time

--- src/easy-collection/array/best-time-to-buy-and-sell-stock-II/solution.ts
+++ src/easy-collection/array/best-time-to-buy-and-sell-stock-II/solution.ts
@@ -1,30 +1,40 @@
+// export function maxProfit(prices: number[]): number {
+//   let profit = 0;
+//   let buyIndex = 0;
+//   let sellIndex = 0;
+//   do {
+//     buyIndex = getLocalMin(prices, buyIndex);
+//     sellIndex = getLocalMax(prices, buyIndex);
+//     profit += prices[sellIndex] - prices[buyIndex];
+//     buyIndex = sellIndex;
+//   } while (sellIndex < prices.length - 1);
+//   return profit;
+// }
+
+// function getLocalMin(prices: number[], startIndex: number): number {
+//   for (let index = startIndex; index < prices.length - 1; index++) {
+//     if (prices[index + 1] > prices[index]) {
+//       return index;
+//     }
+//   }
+//   return prices.length - 1;
+// }
+
+// function getLocalMax(prices: number[], startIndex: number): number {
+//   for (let index = startIndex; index < prices.length; index++) {
+//     if (prices[index + 1] < prices[index]) {
+//       return index;
+//     }
+//   }
+//   return prices.length - 1;
+// }
+
 export function maxProfit(prices: number[]): number {
   let profit = 0;
-  let buyIndex = 0;
-  let sellIndex = 0;
-  do {
-    buyIndex = getLocalMin(prices, buyIndex);
-    sellIndex = getLocalMax(prices, buyIndex);
-    profit += prices[sellIndex] - prices[buyIndex];
-    buyIndex = sellIndex;
-  } while (sellIndex < prices.length - 1);
+  for (let i = 0; i < prices.length; i += 1) {
+    const now = prices[i];
+    const tomorrow = prices[i + 1];
+    if (now < tomorrow) profit += tomorrow - now;
+  }
   return profit;
-}
-
-function getLocalMin(prices: number[], startIndex: number): number {
-  for (let index = startIndex; index < prices.length - 1; index++) {
-    if (prices[index + 1] > prices[index]) {
-      return index;
-    }
-  }
-  return prices.length - 1;
-}
-
-function getLocalMax(prices: number[], startIndex: number): number {
-  for (let index = startIndex; index < prices.length; index++) {
-    if (prices[index + 1] < prices[index]) {
-      return index;
-    }
-  }
-  return prices.length - 1;
 }
