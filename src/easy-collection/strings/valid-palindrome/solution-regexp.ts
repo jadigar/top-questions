8e89cfd2c579235052a4f515a91ad3d0364ded2d
jadigar <whakawa@outlook.com>
2024-04-11 22:47:05 +0500

    valid palindrome

--- src/easy-collection/strings/valid-palindrome/solution-regexp.ts
+++ src/easy-collection/strings/valid-palindrome/solution-regexp.ts
@@ -1,22 +1,27 @@
-export function isPalindrome(s: string): boolean {
-  const isAlphaNumeric = /[a-zA-Z0-9]/;
-  let li = 0;
-  let ri = s.length - 1;
+// export function isPalindrome(s: string): boolean {
+//   const isAlphaNumeric = /[a-zA-Z0-9]/;
+//   let li = 0;
+//   let ri = s.length - 1;
 
-  while (li <= ri) {
-    if (!isAlphaNumeric.test(s[li])) {
-      li++;
-      continue;
-    }
-    if (!isAlphaNumeric.test(s[ri])) {
-      ri--;
-      continue;
-    }
-    if (s[li].toLowerCase() !== s[ri].toLowerCase()) {
-      return false;
-    }
-    li++;
-    ri--;
-  }
-  return true;
+//   while (li <= ri) {
+//     if (!isAlphaNumeric.test(s[li])) {
+//       li++;
+//       continue;
+//     }
+//     if (!isAlphaNumeric.test(s[ri])) {
+//       ri--;
+//       continue;
+//     }
+//     if (s[li].toLowerCase() !== s[ri].toLowerCase()) {
+//       return false;
+//     }
+//     li++;
+//     ri--;
+//   }
+//   return true;
+// }
+
+export function isPalindrome(s: string): boolean {
+  const v = s.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+  return [...v].reverse().join('') === v;
 }
