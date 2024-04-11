d77da6e7741fab9c0e43fd62e86154678ed0648f
jadigar <whakawa@outlook.com>
2024-04-11 22:46:35 +0500

    reverse-string

--- src/easy-collection/strings/reverse-string/solution.ts
+++ src/easy-collection/strings/reverse-string/solution.ts
@@ -1,10 +1,34 @@
 /**
  Do not return anything, modify s in-place instead.
  */
+// export function reverseString(s: string[]): void {
+//   for (let i = 0; i < s.length / 2; i++) {
+//     const temp = s[i];
+//     s[i] = s[s.length - 1 - i];
+//     s[s.length - 1 - i] = temp;
+//   }
+// }
+
 export function reverseString(s: string[]): void {
-  for (let i = 0; i < s.length / 2; i++) {
+  let i = 0;
+  let l = s.length - 1;
+  while (i <= l) {
     const temp = s[i];
-    s[i] = s[s.length - 1 - i];
-    s[s.length - 1 - i] = temp;
+    s[i] = s[l];
+    s[l] = temp;
+    i += 1;
+    l -= 1;
   }
 }
+
+// #TODO: need fix
+// export function reverseString(s: string[]): void {
+//   let i = 0;
+//   let l = s.length - 1;
+//   while (--l) {
+//     const temp = s[i];
+//     s[i] = s[l];
+//     s[l] = temp;
+//     i += 1;
+//   }
+// }
