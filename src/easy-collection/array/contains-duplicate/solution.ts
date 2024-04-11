2ccfe3fe366483a81921876e7b3a2bdc39724b95
jadigar <whakawa@outlook.com>
2024-04-11 22:46:22 +0500

    contains-duplicate

--- src/easy-collection/array/contains-duplicate/solution.ts
+++ src/easy-collection/array/contains-duplicate/solution.ts
@@ -1,15 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 // Sort solution
-export function containsDuplicate(nums: number[]): boolean {
-  nums.sort();
-  for (let index = 0; index < nums.length - 1; index++) {
-    if (nums[index] === nums[index + 1]) {
-      return true;
-    }
-  }
-  return false;
-}
+// export function containsDuplicate(nums: number[]): boolean {
+//   nums.sort();
+//   for (let index = 0; index < nums.length - 1; index++) {
+//     if (nums[index] === nums[index + 1]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
 // Set solution
+export function containsDuplicate(nums: number[]): boolean {
+  return new Set(nums).size !== nums.length;
+}
+
 // export function containsDuplicate(nums: number[]): boolean {
-//   return new Set(nums).size !== nums.length;
+//   const set = new Set();
+//   for (let i = 0; i < nums.length; i++) {
+//     if (set.has(nums[i])) return true;
+//     set.add(nums[i]);
+//   }
+//   return false;
 // }
