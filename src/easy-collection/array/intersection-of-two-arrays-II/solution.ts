e1aa3eec4b3833c6fdd96405ca67afaba9322c9c
jadigar <whakawa@outlook.com>
2024-04-13 20:14:14 +0500

    intersection-of-two-arrays-II short solution

--- src/easy-collection/array/intersection-of-two-arrays-II/solution.ts
+++ src/easy-collection/array/intersection-of-two-arrays-II/solution.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 // export function intersect(nums1: number[], nums2: number[]): number[] {
 //   const result = [];
 //   const [smaller, bigger] = nums1.length <= nums2.length ? [nums1, nums2] : [nums2, nums1];
@@ -14,21 +15,35 @@
 //   return result;
 // }
 
-export function intersect(nums1: number[], nums2: number[]): number[] {
-  const [smaller, bigger] = nums1.length <= nums2.length ? [nums1, nums2] : [nums2, nums1];
-  const smallerMap = smaller.reduce((prev, current) => {
-    if (!prev.has(current)) prev.set(current, 0);
-    prev.set(current, (prev.get(current) ?? 0) + 1);
-    return prev;
-  }, new Map<number, number>());
+// export function intersect(nums1: number[], nums2: number[]): number[] {
+//   const [smaller, bigger] = nums1.length <= nums2.length ? [nums1, nums2] : [nums2, nums1];
+//   const smallerMap = smaller.reduce((prev, current) => {
+//     prev.set(current, (prev.get(current) ?? 0) + 1);
+//     return prev;
+//   }, new Map<number, number>());
 
-  const inter: number[] = [];
-  bigger.forEach((value) => {
-    const count = smallerMap.get(value) ?? 0;
-    if (count) {
-      inter.push(value);
-      smallerMap.set(value, count - 1);
+//   const inter: number[] = [];
+//   bigger.forEach((value) => {
+//     const count = smallerMap.get(value) ?? 0;
+//     if (count) {
+//       inter.push(value);
+//       smallerMap.set(value, count - 1);
+//     }
+//   });
+//   return inter;
+// }
+
+export function intersect(nums1: number[], nums2: number[]): number[] {
+  const numMap: Record<number, number> = {};
+  for (const num of nums1) {
+    numMap[num] = (numMap[num] || 0) + 1;
+  }
+  const result: number[] = [];
+  for (const num of nums2) {
+    if (numMap[num] > 0) {
+      result.push(num);
+      numMap[num]--;
     }
-  });
-  return inter.sort((a, b) => a - b);
+  }
+  return result;
 }
--- src/easy-collection/array/intersection-of-two-arrays-II/test.spec.ts
+++ src/easy-collection/array/intersection-of-two-arrays-II/test.spec.ts
@@ -11,7 +11,7 @@ it('intersect should return [4,9] for nums1 = [4,9,5], nums2 = [9,4,9,8,4]', () 
   const nums1 = [4, 9, 5];
   const nums2 = [9, 4, 9, 8, 4];
   const result = intersect(nums1, nums2);
-  expect(result).toEqual([4, 9]);
+  expect(result).toEqual([9, 4]);
 });
 
 it('intersect should return [2,3,9,13] for nums1 = [2,2,2,3,3,9,9,13] nums2 = [2,3,9,13,13]', () => {
