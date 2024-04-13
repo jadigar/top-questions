382189fdeb9bd3d696f1475ef81dcfc80e3da7fb
jadigar <whakawa@outlook.com>
2024-04-13 18:24:52 +0500

    plus-one

--- src/easy-collection/array/plus-one/solution.ts
+++ src/easy-collection/array/plus-one/solution.ts
@@ -1,13 +1,29 @@
+/* eslint-disable no-param-reassign */
+// export function plusOne(digits: number[]): number[] {
+//   let lastIndex = digits.length - 1;
+//   while (digits[lastIndex] === 9) {
+//     digits[lastIndex] = 0;
+//     if (lastIndex !== 0) {
+//       lastIndex--;
+//     } else if (lastIndex === 0) {
+//       digits.unshift(0);
+//     }
+//   }
+//   digits[lastIndex]++;
+//   return digits;
+// }
+
 export function plusOne(digits: number[]): number[] {
-  let lastIndex = digits.length - 1;
-  while (digits[lastIndex] === 9) {
-    digits[lastIndex] = 0;
-    if (lastIndex !== 0) {
-      lastIndex--;
-    } else if (lastIndex === 0) {
-      digits.unshift(0);
+  let l = digits.length;
+  while (l--) {
+    const digit = digits[l];
+    if (digit === 9) {
+      digits[l] = 0;
+      if (!l) { digits.unshift(1); }
+    } else {
+      digits[l] = digit + 1;
+      break;
     }
   }
-  digits[lastIndex]++;
   return digits;
 }
