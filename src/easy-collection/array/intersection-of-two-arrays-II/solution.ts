801c6194a075a90719fa07bd1b088a5cd5fc7dce
jadigar <whakawa@outlook.com>
2024-04-13 17:34:43 +0500

    intersection-of-two-arrays-II

--- src/easy-collection/array/intersection-of-two-arrays-II/solution.ts
+++ src/easy-collection/array/intersection-of-two-arrays-II/solution.ts
@@ -1,15 +1,34 @@
-export function intersect(nums1: number[], nums2: number[]): number[] {
-  const result = [];
-  const [smaller, bigger] = nums1.length <= nums2.length ? [nums1, nums2] : [nums2, nums1];
-  smaller.sort((a, b) => a - b);
-  bigger.sort((a, b) => a - b);
-  for (let i = 0, k = 0; i < smaller.length; i++) {
-    const idx = bigger.indexOf(smaller[i], k);
-    if (idx !== -1) {
-      result.push(smaller[i]);
-      k = idx + 1;
-    }
-  }
+// export function intersect(nums1: number[], nums2: number[]): number[] {
+//   const result = [];
+//   const [smaller, bigger] = nums1.length <= nums2.length ? [nums1, nums2] : [nums2, nums1];
+//   smaller.sort((a, b) => a - b);
+//   bigger.sort((a, b) => a - b);
+//   for (let i = 0, k = 0; i < smaller.length; i++) {
+//     const idx = bigger.indexOf(smaller[i], k);
+//     if (idx !== -1) {
+//       result.push(smaller[i]);
+//       k = idx + 1;
+//     }
+//   }
 
-  return result;
+//   return result;
+// }
+
+export function intersect(nums1: number[], nums2: number[]): number[] {
+  const [smaller, bigger] = nums1.length <= nums2.length ? [nums1, nums2] : [nums2, nums1];
+  const smallerMap = smaller.reduce((prev, current) => {
+    if (!prev.has(current)) prev.set(current, 0);
+    prev.set(current, (prev.get(current) ?? 0) + 1);
+    return prev;
+  }, new Map<number, number>());
+
+  const inter: number[] = [];
+  bigger.forEach((value) => {
+    const count = smallerMap.get(value) ?? 0;
+    if (count) {
+      inter.push(value);
+      smallerMap.set(value, count - 1);
+    }
+  });
+  return inter.sort((a, b) => a - b);
 }
